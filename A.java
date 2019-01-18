7434171709f3c89f43c0e3c32cbd5916d360d7ba
ArchsM <43184231+ArchsM@users.noreply.github.com>
2019-01-18 14:38:56 -0600

    Update A.java

--- A.java
+++ A.java
@@ -1,10 +1,3 @@
-import java.util.Scanner;
-/**
- * Write a description of class A here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
 public class A
 {
     public static Scanner Arch = new Scanner(System.in);
