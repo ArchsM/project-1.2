6869e9ae8f3c12b69bcb808a030cc4672ad9a1f8
ArchsM <43184231+ArchsM@users.noreply.github.com>
2019-01-18 14:38:21 -0600

    Add files via upload

---
+++ A.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+/**
+ * Write a description of class A here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class A
+{
+    public static Scanner Arch = new Scanner(System.in);
+    public static void main(String[]args){
+        System.out.println("What program do you want?");
+        String = Arch.nextLine();
+        
+    }
+}
